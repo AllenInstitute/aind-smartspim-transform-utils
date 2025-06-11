f7af02104b2edd5e27f99800c63999c389a89557
nal10 <nal10@users.noreply.github.com>
2025-06-11 13:41:34 -0700

    Update __init__.py
    possible CO install option

--- src/aind_smartspim_transform_utils/__init__.py
+++ src/aind_smartspim_transform_utils/__init__.py
@@ -1,6 +1,6 @@
 """ Init package """
 
-
+import os
 from pathlib import Path
 
 __version__ = "1.0.0"
@@ -9,3 +9,12 @@ __version__ = "1.0.0"
 # where files will be downloaded if needed
 base_dir = Path.home() / ".transform_utils" / "transform_utils"
 base_dir.mkdir(parents=True, exist_ok=True)
+
+# this code would install in scratch if on CO but dont know if it is good for local
+
+#if os.path.exists(Path.home() / "capsule"):
+#    base_dir = Path.home() / 'capsule' / 'scratch' / "transform_utils"
+#else:
+#    base_dir = Path.home() / ".transform_utils" / "transform_utils"
+
+#base_dir.mkdir(parents=True, exist_ok=True)
